ab2b9944501f99f70d367a867ca866d4a84a1a5e
CristianMarch <kicho2002@hotmail.com>
2023-08-09 11:27:08 -0300

    nuevo cambio

--- Formulario/src/formulario/ClasedeCristian.java
+++ Formulario/src/formulario/ClasedeCristian.java
@@ -11,5 +11,6 @@ package formulario;
 public class ClasedeCristian {
     public static void main(String[] args) {
         System.out.println("HOLAaa");
+        System.out.println("NUEVO CAMBIO");
     }
 }
